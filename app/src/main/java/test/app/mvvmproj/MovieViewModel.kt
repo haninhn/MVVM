f10c4191cbd52c101f66eafcadbf5a6a0da415e2
hanin guesmi <haninhn@users.noreply.github.com>
2022-01-02 14:53:50 +0100

    add ViewModel class

--- app/src/main/java/test/app/mvvmproj/MovieViewModel.kt
+++ app/src/main/java/test/app/mvvmproj/MovieViewModel.kt
@@ -1,19 +1,17 @@
 package test.app.mvvmproj
 
-import android.view.ViewGroup
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.newFixedThreadPoolContext
 
 class MovieViewModel: ViewModel() {
 
     //parametre fih il movie name ykon live bach il ui kif ytab3ha yl9aha 93da tt4ayr bchkil mobachir
-     var MovieNameMutableLiveData : MutableLiveData<String> = MutableLiveData()
+     val movieNameMutableLiveData : MutableLiveData<String> = MutableLiveData()
     //lfar9 bin live data w MutableLiveData enti il MutableLiveData njmo nbdlo valeur mat3ha manuellement
 
      fun getMovieName(){
-        var MovieName = getMovieFromDB().movieName
-        MovieNameMutableLiveData.value = MovieName
+        val movieName = getMovieFromDB().movieName
+        movieNameMutableLiveData.value = movieName
     }
    private fun getMovieFromDB(): moviModel {
         return moviModel("noura")
